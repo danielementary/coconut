cd10e23b8c8b0e0e54b7349dd147b860ddd1d42c
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2021-10-20 16:24:51 +0200

    add e2e test for set membership

--- coconut-rs/src/tests/e2e.rs
+++ coconut-rs/src/tests/e2e.rs
@@ -1,7 +1,8 @@
 use crate::{
-    aggregate_signature_shares, aggregate_verification_keys, blind_sign, CoconutError,
-    elgamal_keygen, prepare_blind_sign, prove_credential, setup, Signature, SignatureShare,
-    ttp_keygen, VerificationKey, verify_credential,
+    aggregate_signature_shares, aggregate_verification_keys, blind_sign, elgamal_keygen,
+    issue_membership_signatures, prepare_blind_sign, prove_credential, setup, ttp_keygen,
+    utils::RawAttribute, verify_credential, CoconutError, Signature, SignatureShare,
+    VerificationKey,
 };
 
 #[test]
@@ -51,7 +52,18 @@ fn main() -> Result<(), CoconutError> {
     let unblinded_signatures: Vec<Signature> = blinded_signatures
         .into_iter()
         .zip(verification_keys.iter())
-        .map(|(signature, verification_key)| signature.unblind(&params, &elgamal_keypair.private_key(), &verification_key, &private_attributes, &public_attributes, blind_sign_request.commitment_hash).unwrap())
+        .map(|(signature, verification_key)| {
+            signature
+                .unblind(
+                    &params,
+                    &elgamal_keypair.private_key(),
+                    &verification_key,
+                    &private_attributes,
+                    &public_attributes,
+                    blind_sign_request.commitment_hash,
+                )
+                .unwrap()
+        })
         .collect();
 
     // Aggregate signatures
@@ -67,7 +79,8 @@ fn main() -> Result<(), CoconutError> {
     attributes.extend_from_slice(&public_attributes);
 
     // Randomize credentials and generate any cryptographic material to verify them
-    let signature = aggregate_signature_shares(&params, &verification_key, &attributes, &signature_shares)?;
+    let signature =
+        aggregate_signature_shares(&params, &verification_key, &attributes, &signature_shares)?;
 
     // Generate cryptographic material to verify them
 
@@ -75,6 +88,108 @@ fn main() -> Result<(), CoconutError> {
 
     // Verify credentials
 
+    assert!(verify_credential(
+        &params,
+        &verification_key,
+        &theta,
+        &public_attributes,
+    ));
+
+    Ok(())
+}
+
+#[test]
+fn main_set_membership() -> Result<(), CoconutError> {
+    let params = setup(1).unwrap();
+
+    // build dummy private attribute 1
+    let private_attribute = 1;
+    let private_attributes = [RawAttribute::Number(private_attribute).into()];
+    let public_attributes = params.n_random_scalars(0);
+
+    let elgamal_keypair = elgamal_keygen(&params);
+
+    // issue signatures for 0, 1 and 2
+    let phi = [
+        RawAttribute::Number(0),
+        RawAttribute::Number(1),
+        RawAttribute::Number(2),
+    ];
+    let membership_signatures = issue_membership_signatures(&params, &phi);
+
+    // generate commitment and encryption
+    let blind_sign_request = prepare_blind_sign(
+        &params,
+        &elgamal_keypair,
+        &private_attributes,
+        &public_attributes,
+    )?;
+
+    // generate_keys
+    let coconut_keypairs = ttp_keygen(&params, 2, 3).unwrap();
+
+    let verification_keys: Vec<VerificationKey> = coconut_keypairs
+        .iter()
+        .map(|keypair| keypair.verification_key())
+        .collect();
+
+    // aggregate verification keys
+    let verification_key = aggregate_verification_keys(&verification_keys, Some(&[1, 2, 3]))?;
+
+    // generate blinded signatures
+    let mut blinded_signatures = Vec::new();
+
+    for keypair in coconut_keypairs {
+        let blinded_signature = blind_sign(
+            &params,
+            &keypair.secret_key(),
+            &elgamal_keypair.public_key(),
+            &blind_sign_request,
+            &public_attributes,
+        )?;
+        blinded_signatures.push(blinded_signature)
+    }
+
+    // Unblind signatures
+    let unblinded_signatures: Vec<Signature> = blinded_signatures
+        .into_iter()
+        .zip(verification_keys.iter())
+        .map(|(signature, verification_key)| {
+            signature
+                .unblind(
+                    &params,
+                    &elgamal_keypair.private_key(),
+                    &verification_key,
+                    &private_attributes,
+                    &public_attributes,
+                    blind_sign_request.commitment_hash,
+                )
+                .unwrap()
+        })
+        .collect();
+
+    // Aggregate signatures
+    let signature_shares: Vec<SignatureShare> = unblinded_signatures
+        .iter()
+        .enumerate()
+        .map(|(idx, signature)| SignatureShare::new(*signature, (idx + 1) as u64))
+        .collect();
+
+    let mut attributes = Vec::with_capacity(private_attributes.len() + public_attributes.len());
+    attributes.extend_from_slice(&private_attributes);
+    attributes.extend_from_slice(&public_attributes);
+
+    // pick the membership signature corresponding to the private attribute
+    let membership_signature = membership_signatures.get(&RawAttribute::Number(private_attribute));
+
+    // Randomize credentials and generate any cryptographic material to verify them
+    let signature =
+        aggregate_signature_shares(&params, &verification_key, &attributes, &signature_shares)?;
+
+    // Generate cryptographic material to verify them
+    let theta = prove_credential(&params, &verification_key, &signature, &private_attributes)?;
+
+    // Verify credentials
     assert!(verify_credential(
         &params,
         &verification_key,
