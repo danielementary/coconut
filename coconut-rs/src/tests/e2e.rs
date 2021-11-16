188e314c6ced8e671d306c9e3668af595800e37c
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2021-11-16 15:17:56 +0100

    add e2e for set membership private attribute out of set

--- coconut-rs/src/tests/e2e.rs
+++ coconut-rs/src/tests/e2e.rs
@@ -214,6 +214,124 @@ fn main_set_membership() -> Result<(), CoconutError> {
 }
 
 #[test]
+#[should_panic]
+fn main_out_of_set_membership() {
+    let params = setup(1).unwrap();
+
+    // build dummy private attribute 1
+    let private_attribute = 3;
+    let private_attributes = [RawAttribute::Number(private_attribute).into()];
+    let public_attributes = params.n_random_scalars(0);
+
+    let elgamal_keypair = elgamal_keygen(&params);
+
+    // issue signatures for 0, 1 and 2
+    let set = [
+        RawAttribute::Number(0),
+        RawAttribute::Number(1),
+        RawAttribute::Number(2),
+    ];
+    let membership_signatures = issue_membership_signatures(&params, &set);
+
+    // generate commitment and encryption
+    let blind_sign_request = prepare_blind_sign(
+        &params,
+        &elgamal_keypair,
+        &private_attributes,
+        &public_attributes,
+    )
+    .unwrap();
+
+    // generate_keys
+    let coconut_keypairs = ttp_keygen(&params, 2, 3).unwrap();
+
+    let verification_keys: Vec<VerificationKey> = coconut_keypairs
+        .iter()
+        .map(|keypair| keypair.verification_key())
+        .collect();
+
+    // aggregate verification keys
+    let verification_key =
+        aggregate_verification_keys(&verification_keys, Some(&[1, 2, 3])).unwrap();
+
+    // generate blinded signatures
+    let mut blinded_signatures = Vec::new();
+
+    for keypair in coconut_keypairs {
+        let blinded_signature = blind_sign(
+            &params,
+            &keypair.secret_key(),
+            &elgamal_keypair.public_key(),
+            &blind_sign_request,
+            &public_attributes,
+        )
+        .unwrap();
+        blinded_signatures.push(blinded_signature)
+    }
+
+    // Unblind signatures
+    let unblinded_signatures: Vec<Signature> = blinded_signatures
+        .into_iter()
+        .zip(verification_keys.iter())
+        .map(|(signature, verification_key)| {
+            signature
+                .unblind(
+                    &params,
+                    &elgamal_keypair.private_key(),
+                    &verification_key,
+                    &private_attributes,
+                    &public_attributes,
+                    blind_sign_request.commitment_hash,
+                )
+                .unwrap()
+        })
+        .collect();
+
+    // Aggregate signatures
+    let signature_shares: Vec<SignatureShare> = unblinded_signatures
+        .iter()
+        .enumerate()
+        .map(|(idx, signature)| SignatureShare::new(*signature, (idx + 1) as u64))
+        .collect();
+
+    let mut attributes = Vec::with_capacity(private_attributes.len() + public_attributes.len());
+    attributes.extend_from_slice(&private_attributes);
+    attributes.extend_from_slice(&public_attributes);
+
+    let _signature =
+        aggregate_signature_shares(&params, &verification_key, &attributes, &signature_shares)
+            .unwrap();
+
+    // pick the membership signature corresponding to the private attribute
+    let _membership_signature = membership_signatures
+        .signatures
+        .get(&RawAttribute::Number(private_attribute))
+        .unwrap();
+    // let sp_verification_key = membership_signatures.sp_verification_key;
+
+    // // Randomize credentials and generate any cryptographic material to verify them
+    // let theta = prove_credential_and_set_membership(
+    //     &params,
+    //     &verification_key,
+    //     &sp_verification_key,
+    //     &signature,
+    //     &membership_signature,
+    //     &private_attributes,
+    // )?;
+
+    // // Verify credentials
+    // assert!(verify_set_membership_credential(
+    //     &params,
+    //     &verification_key,
+    //     &sp_verification_key,
+    //     &theta,
+    //     &public_attributes,
+    // ));
+
+    // Ok(())
+}
+
+#[test]
 fn main_range() -> Result<(), CoconutError> {
     let params = setup(1).unwrap();
 
