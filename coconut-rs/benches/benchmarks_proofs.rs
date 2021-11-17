f511b0c2489cb2642f8dee58f987dcc923db6495
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2021-11-17 15:37:46 +0100

    add benchmarks for SetMembershipTheta and RangeTheta to bytes and back

--- coconut-rs/benches/benchmarks_proofs.rs
+++ coconut-rs/benches/benchmarks_proofs.rs
@@ -1,15 +1,16 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
 use core::time::Duration;
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 use std::mem;
 
 use rand::Rng;
 
 use bls12_381::Scalar;
 use coconut_rs::{
-    compute_u_ary_decomposition, issue_membership_signatures, issue_range_signatures,
-    pick_range_signatures, setup, RawAttribute, L, U,
+    compute_u_ary_decomposition, issue_membership_signatures, issue_range_signatures, keygen,
+    pick_range_signatures, prove_credential_and_range, prove_credential_and_set_membership, setup,
+    single_attribute_keygen, RangeTheta, RawAttribute, SetMembershipTheta, L, U,
 };
 
 pub fn bench_proofs_functions(c: &mut Criterion) {
@@ -61,6 +62,59 @@ pub fn bench_proofs_functions(c: &mut Criterion) {
     group.finish();
 
     // TODO: add roundtrips for SMP, RP, SMT and RT
+    //
+    // prepare roundtrips benchmarks with dummy values
+    let params = setup(10).unwrap();
+
+    let verification_key = keygen(&params).verification_key();
+    let sp_verification_key = single_attribute_keygen(&params).verification_key();
+    let private_attributes = params.n_random_scalars(5);
+
+    let signature = range_signatures
+        .signatures
+        .get(&RawAttribute::Number(0))
+        .unwrap();
+    let membership_signature = range_signatures
+        .signatures
+        .get(&RawAttribute::Number(0))
+        .unwrap();
+
+    let set_membership_theta = prove_credential_and_set_membership(
+        &params,
+        &verification_key,
+        &sp_verification_key,
+        &signature,
+        &membership_signature,
+        &private_attributes,
+    )
+    .unwrap();
+
+    // benchmark how long it takes to convert SetMembershipTheta to bytes and back
+    c.bench_function("convert SetMembershipTheta to bytes and back", |b| {
+        b.iter(|| {
+            SetMembershipTheta::try_from(set_membership_theta.to_bytes().as_slice()).unwrap()
+        });
+    });
+
+    let a = Scalar::from(0);
+    let b = Scalar::from(15);
+
+    let range_theta = prove_credential_and_range(
+        &params,
+        &verification_key,
+        &sp_verification_key,
+        &signature,
+        &range_signatures,
+        a,
+        b,
+        &private_attributes,
+    )
+    .unwrap();
+
+    // benchmark how long it takes to convert RangeTheta to bytes and back
+    c.bench_function("convert RangeTheta to bytes and back", |b| {
+        b.iter(|| RangeTheta::try_from(range_theta.to_bytes().as_slice()).unwrap());
+    });
 }
 
 criterion_group!(benches_proof, bench_proofs_functions);
